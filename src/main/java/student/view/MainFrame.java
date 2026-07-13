ea45444876520352abd2015e5534079412c5e25d
cbourgeois <bourgeois.car@northeastern.edu>
2026-07-13 15:56:53 -0400

    Task 2: MainFrame.java #4 add result display area

--- src/main/java/student/view/MainFrame.java
+++ src/main/java/student/view/MainFrame.java
@@ -14,6 +14,18 @@ public class MainFrame extends JFrame {
     private JTextField enterHostname;
     /** button for lookup. */
     private JButton lookupButton;
+    /** label for hostname. */
+    private JLabel hostnameLabel;
+    /** label for IP. */
+    private JLabel ipLabel;
+    /** label for city. */
+    private JLabel cityLabel;
+    /** label for region. */
+    private JLabel regionLabel;
+    /** label for country. */
+    private JLabel countryLabel;
+    /** label for coordinates.  */
+    private JLabel coordinatesLabel;
 
     /**
      * constructor for building and displaying window.
@@ -45,8 +57,36 @@ public class MainFrame extends JFrame {
             String hostname = enterHostname.getText();
             // View calls Controller.
             DomainNameModel.DNRecord record = controller.lookupHostname(hostname);
-            System.out.println(record);
+            // check to make sure record not null.
+            if (record != null) {
+                hostnameLabel.setText("Hostname: " + record.hostname());
+                ipLabel.setText("IP: " + record.ip());
+                cityLabel.setText("City: " + record.city());
+                regionLabel.setText("Region: " + record.region());
+                countryLabel.setText("Country: " + record.country());
+                coordinatesLabel.setText("Coordinates: " + record.latitude() + ", " + record.longitude());
+            } else {
+                hostnameLabel.setText("Hostname not found/invalid.");
+            }
         });
+
+        // set up for result panel.
+        JPanel resultPanel = new JPanel(new GridLayout(6, 1));
+        hostnameLabel = new JLabel("Hostname: ");
+        ipLabel = new JLabel("IP: ");
+        cityLabel = new JLabel("City: ");
+        regionLabel = new JLabel("Region: ");
+        countryLabel = new JLabel("Country: ");
+        coordinatesLabel = new JLabel("Coordinates: ");
+
+        // add result panel.
+        resultPanel.add(hostnameLabel);
+        resultPanel.add(ipLabel);
+        resultPanel.add(cityLabel);
+        resultPanel.add(regionLabel);
+        resultPanel.add(countryLabel);
+        resultPanel.add(coordinatesLabel);
+        add(resultPanel, BorderLayout.CENTER);
     }
 
     /**
